2d9b4edae32d8ac942b3f102751b3363ba4329c5
Jaron Zijlstra <jaron_zijlstra@outlook.com>
2025-02-08 17:54:34 +0100

    Update install.lua

--- bash/install.lua
+++ bash/install.lua
@@ -2,29 +2,32 @@ local repoURL = "https://raw.githubusercontent.com/JaronZ/CCTweaked-Scripts/main
 local libLoc = "lib/jaronz/"
 local libRoot = repoURL..libLoc
 local programRoot = repoURL.."bash/src/"
-
+ 
 local libFiles = {"program.lua", "file.lua"}
 local programFiles = {"cat.lua", "touch.lua", "echo.lua"}
-
+ 
 fs.makeDir(libLoc)
-
+ 
 for i = 1, #libFiles do
     local fileName = libFiles[i]
-
+ 
     -- Remove if already exists, so it can be replaced with a possible newer version
     if fs.exists(libLoc..fileName) then
+        print("Updating '"..libLoc..fileName.."'...")
         fs.delete(libLoc..fileName)
     end
-
+ 
     local libURL = libRoot..fileName
     local libFile = http.get(libURL).readAll()
-
-    fs.open(libLoc..fileName, "w").write(libFile).close()
+ 
+    local file = fs.open(libLoc..fileName, "w")
+    file.write(libFile)
+    file.close()
 end
-
+ 
 for i = 1, #programFiles do
     local fileName = programFiles[i]
-
+ 
     if fs.exists(fileName) then
         print("warning: program '"..fileName.."' already exists")
         print("do you want to overwrite this file? (Y/N)")
@@ -34,11 +37,13 @@ for i = 1, #programFiles do
         end
         fs.delete(fileName)
     end
-
+ 
     local programURL = programRoot..fileName
     local programFile = http.get(programURL).readAll()
-
-    fs.open(fileName, "w").write(programFile).close()
-
+ 
+    local file = fs.open(fileName, "w")
+    file.write(programFile)
+    file.close()
+ 
     ::endDownload::
 end
